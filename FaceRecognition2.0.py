c18f4b47b544d6bab222d70f827fe039309bccea
Bram Van Reusel <bram.vanreusel@student.kuleuven.be>
2021-10-08 17:44:59 +0200

    e

---
+++ FaceRecognition2.0.py
@@ -0,0 +1,9 @@
+from imutils import paths
+import pickle
+import cv2
+import os
+
+
+# get paths of each file in folder named Images
+# Images here contains my data(folders of various persons)
+print(os.listdir(r"C:\Users\bram\testfolder"))
