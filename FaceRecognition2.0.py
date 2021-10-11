0f74eba2b48e294958fc3afeab71d9cfba67b221
Bram Van Reusel <bram.vanreusel@student.kuleuven.be>
2021-10-11 12:43:32 +0200

    Got it working till the Eigenfaces part

--- FaceRecognition2.0.py
+++ FaceRecognition2.0.py
@@ -3,14 +3,17 @@ import pickle
 import cv2
 import os
 import numpy
+from scipy import linalg
+from scipy.sparse.linalg import norm
 
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
 cam=cv2.VideoCapture(0)
 directory = r'C:\Users\bram\testfolder\s'
 
+
 def imagesizeconverter (x,y,w,h):
-    width = 60
-    height = 80
+    width = 45
+    height = 60
     if w != width or h != height:
         # We are gonna be using x+a and w-a to keep the center of the face in the center,
         # so now we have to find a to convert
@@ -21,30 +24,33 @@ def imagesizeconverter (x,y,w,h):
         y += b
     return [int(x), int(y), int(x+width), int(y+height)]
 
+
 imagematrix = ""
 for i in os.listdir(r"C:\Users\bram\testfolder"):
     filenamewithouts = i[1:]
-    image_vector = numpy.array(cv2.imread(directory + str(filenamewithouts))).reshape(-1)
+    image = cv2.imread(directory + str(filenamewithouts))
+    image_vector = numpy.array(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)).reshape(-1)
     if isinstance(imagematrix, str):
         imagematrix = numpy.matrix(image_vector)
     else:
         imagematrix = numpy.append(imagematrix, [image_vector], axis=0)
 averageface = numpy.sum(imagematrix, axis=0)/len(os.listdir(r"C:\Users\bram\testfolder"))
-
 variancematrix = ""
 for i in os.listdir(r"C:\Users\bram\testfolder"):
     filenamewithouts = i[1:]
-    image_vector = numpy.array(cv2.imread(directory + str(filenamewithouts))).reshape(-1)
+    image = cv2.imread(directory + str(filenamewithouts))
+    image_vector = numpy.array(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)).reshape(-1)
     if isinstance(variancematrix, str):
-        variancematrix = numpy.matrix(image_vector-averageface[0])
+        variancematrix = numpy.matrix(image_vector - averageface[0])
     else:
-        subtraction = image_vector-averageface[0]
+        subtraction = image_vector - averageface[0]
         variancematrix = numpy.append(variancematrix, subtraction, axis=0)
 covariancematrix = numpy.dot(variancematrix.transpose(), variancematrix)
-print("hello1")
-P, D, V = numpy.linalg.svd(covariancematrix)
+P, D, V = linalg.svd(covariancematrix)
 D = numpy.diag(D)
 Eigenfaces = numpy.dot(variancematrix, numpy.dot(V, numpy.power(D, (-1/2))))
+print(Eigenfaces)
+
 
 # get paths of each file in folder named Images
 # Images here contains my data(folders of various persons)
@@ -58,11 +64,11 @@ while True:
         convertedimage = imagesizeconverter(x, y, w, h)
         vectornewface = numpy.array(gray[convertedimage[1]:convertedimage[3],
                                         convertedimage[0]:convertedimage[2]]).reshape(-1)
-        weightvectornewface = numpy.dot(eigenfaces.transpose(), vectornewface - averageface)
+        weightvectornewface = numpy.dot(Eigenfaces.transpose(), vectornewface - averageface)
         predefined_treshhold = 1000;
         match = 0;
         for i in range(len(os.listdir(r"C:\Users\bram\testfolder"))):
-            euclidian_distance[i] = numpy.linalg.norm(weightvectornewface - (Eigenfaces.transpose()*difference_with_average_face[:,i]));
+            euclidian_distance[i] = norm(weightvectornewface - (numpy.dot(Eigenfaces.transpose(), variancematrix[:, i])));
             if euclidian_distance[i] < predefined_treshhold:
                 match += 1
             if match == len(os.listdir(r"C:\Users\bram\testfolder")):
