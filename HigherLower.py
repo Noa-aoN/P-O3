ac0022e23529944d0c796ca0c5cdeba332bb2d83
Matthias Van Eysendeyk <matthias.vaneysendeyk@student.kuleuven.be>
2021-11-30 17:46:22 +0100

    flikkerende exit knop

--- HigherLower.py
+++ HigherLower.py
@@ -194,8 +194,6 @@ def higherlower(screen, clock, players, library):
                             lost = True
                             wrong_guess(player1, huidige, screen)
 
-                    elif button_pressed(exit_button, event):
-                        return players
             else:
                 screen.fill((31, 171, 57))
                 again_button.draw(screen)
