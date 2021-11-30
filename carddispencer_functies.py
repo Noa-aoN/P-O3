101099481f5361b1915d7fce254f2bdf75fda4f5
jasperswerts <jasper.swerts@student.kuleuven.be>
2021-11-30 16:08:31 +0100

    carddispenser functies en test aangepast

--- carddispencer_functies.py
+++ carddispencer_functies.py
@@ -1,9 +1,5 @@
-from gpiozero import Servo
 from time import sleep
 import RPi.GPIO as GPIO
-servo = Servo(4)
-
-
 def setup():
     Motor1A = 24
     Motor1B = 23
@@ -13,16 +9,41 @@ def setup():
     GPIO.setup(Motor1B, GPIO.OUT)
     GPIO.setup(Motor1E, GPIO.OUT)
 
-def dcmotor_rotate(amount_of_cards):
+def dcmotor_rotate():
     # Going forwards
     GPIO.output(Motor1A, GPIO.HIGH)
     GPIO.output(Motor1B, GPIO.LOW)
     GPIO.output(Motor1E, GPIO.HIGH)
-    sleep(1*amount_of_cards)
+    sleep(1)
     # Stop
     GPIO.output(Motor1E, GPIO.LOW)
 
 def servo_rotate(player):
-    degree = 180*(player-1)/3
-    p = (degree/180) -0.5
-    servo.value = p
+    servoPIN = 11
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN, GPIO.OUT)
+    pos1 = 300 + (player - 1) * (1020 - 300) / 3
+    p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
+    p.start(2.5)  # Initialization
+    p.ChangeDutyCycle(pos1 / 100)
+    p.stop()
+    GPIO.cleanup()
+
+def servo_rotate_fromto(previous_player,player):
+    servoPIN = 11
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN, GPIO.OUT)
+    pos0 = 300 + (previous_player-1)*(1020-300)/3
+    pos1 = 300 + (player - 1) * (1020 - 300) / 3
+    p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
+    p.start(2.5)  # Initialization
+    if previous_player < 1:
+        p.ChangeDutyCycle(pos1/100)
+
+    for i in range(pos0, pos1):
+        p.ChangeDutyCycle(i / 100)
+        print(i)
+        time.sleep(0.005)
+    p.stop()
+    GPIO.cleanup()
+
--- test_carddispencer.py
+++ test_carddispencer.py
@@ -1,9 +1,12 @@
-from carddispencer_functies import setup, dcmotor_rotate, servo_rotate
+from carddispencer_functies import setup, dcmotor_rotate, servo_rotate , servo_rotate_fromto
 from time import sleep
-servo = Servo(4)
 
 setup()
 for i in range(1,5):
-    servo_rotate(i)
-    dcmotor_rotate(i)
+    if i == 1:
+        servo_rotate(i)
+        dcmotor_rotate()
+    else:
+        ervo_rotate_fromto(i-1,i)
+        dcmotor_rotate()
     sleep(10)
