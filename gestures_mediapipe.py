851b17fa4194457da3847120258a7ea919d29810
Noa <noa.pauwels@gmail.com>
2021-12-06 02:31:53 +0100

    Removed unnecessary print statements

--- gestures_mediapipe.py
+++ gestures_mediapipe.py
@@ -1,8 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
-from Camera import init_camera
-
 """
 kan een heleboel gebaren herkennen via het vergelijken van de coordinaten van de 'landmarks'
 er kunnen nog meer gebaren geimplementeerd worden
@@ -10,7 +7,7 @@ zelfde programma als 'gestures_mediapipe.py' maar dan in een klasse
 link: 
 https://optisol.com.au/insight/alphabet-hand-gestures-recognition-using-mediapipe/#:~:text=MediaPipe%20Hand%20is%20a%20machine%20learning%20employed%20high-fidelity,help%20of%20multiple%20models%20which%20are%20working%20simultaneously.
 """
-FINGERS = ("Higher / One", "Two", "Three", "Four", "Five")
+FINGERS = ("Higher/One", "Two", "Three", "Four", "Five")
 OPTIONS = ("Hit", "Stand", "Double Down")
 THUMB = ("Thumbs Up", "Thumbs Down")
 INDEX = ("Index Up", "Index Down")
@@ -27,7 +24,6 @@ def index_down(hand_landmarks):
             hand_landmarks.landmark[12].y < hand_landmarks.landmark[11].y < hand_landmarks.landmark[10].y and \
             hand_landmarks.landmark[16].y < hand_landmarks.landmark[15].y < hand_landmarks.landmark[14].y and \
             hand_landmarks.landmark[20].y < hand_landmarks.landmark[19].y < hand_landmarks.landmark[18].y:
-        print("lower")
         return True
     return False
 
@@ -37,7 +33,6 @@ def thumbs_up(hand_landmarks):
             hand_landmarks.landmark[9].y > hand_landmarks.landmark[2].y and \
             hand_landmarks.landmark[13].y > hand_landmarks.landmark[2].y and \
             hand_landmarks.landmark[17].y > hand_landmarks.landmark[2].y:
-        print("thumbs up")
         return True
     return False
 
@@ -47,7 +42,6 @@ def thumbs_down(hand_landmarks):
             hand_landmarks.landmark[9].y < hand_landmarks.landmark[2].y and \
             hand_landmarks.landmark[13].y < hand_landmarks.landmark[2].y and \
             hand_landmarks.landmark[17].y < hand_landmarks.landmark[2].y:
-        print("thumbs down")
         return True
     return False
 
@@ -58,7 +52,6 @@ def index_up(hand_landmarks):
             hand_landmarks.landmark[12].y > hand_landmarks.landmark[11].y > hand_landmarks.landmark[10].y and \
             hand_landmarks.landmark[16].y > hand_landmarks.landmark[15].y > hand_landmarks.landmark[14].y and \
             hand_landmarks.landmark[20].y > hand_landmarks.landmark[19].y > hand_landmarks.landmark[18].y:
-        print("higher/one")
         return True
     return False
 
@@ -69,7 +62,6 @@ def fingers_two(hand_landmarks):
             hand_landmarks.landmark[12].y < hand_landmarks.landmark[11].y < hand_landmarks.landmark[10].y and \
             hand_landmarks.landmark[16].y > hand_landmarks.landmark[15].y > hand_landmarks.landmark[14].y and \
             hand_landmarks.landmark[20].y > hand_landmarks.landmark[19].y > hand_landmarks.landmark[18].y:
-        print("two")
         return True
     return False
 
@@ -80,7 +72,6 @@ def fingers_three(hand_landmarks):
             hand_landmarks.landmark[12].y < hand_landmarks.landmark[11].y < hand_landmarks.landmark[10].y and \
             hand_landmarks.landmark[16].y < hand_landmarks.landmark[15].y < hand_landmarks.landmark[14].y and \
             hand_landmarks.landmark[20].y > hand_landmarks.landmark[19].y > hand_landmarks.landmark[18].y:
-        print("three")
         return True
     return False
 
@@ -93,7 +84,6 @@ def fingers_five(hand_landmarks):
             hand_landmarks.landmark[20].y < hand_landmarks.landmark[19].y < hand_landmarks.landmark[18].y and \
             not ((hand_landmarks.landmark[5].x < hand_landmarks.landmark[4].x < hand_landmarks.landmark[17].x) or
                  (hand_landmarks.landmark[17].x < hand_landmarks.landmark[4].x < hand_landmarks.landmark[5].x)):
-        print("five")
         return True
     return False
 
@@ -107,7 +97,6 @@ def fingers_four(hand_landmarks):
             ((hand_landmarks.landmark[5].x < hand_landmarks.landmark[4].x < hand_landmarks.landmark[17].x) or
              (hand_landmarks.landmark[17].x < hand_landmarks.landmark[4].x < hand_landmarks.landmark[5].x)):
         # kijken of duim tussen wijsvinger en pink zit (x-coordinaat)
-        print("four")
         return True
     return False
 
